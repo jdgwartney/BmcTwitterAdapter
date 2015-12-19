c39fc30844802745b9017d4cad7d77cff3535a2d
paul beavers <pbeavers@pbeavers-personal-mini.local>
2015-12-18 21:08:38 -0600

    t

--- evtweet/bin/evtweet.py
+++ evtweet/bin/evtweet.py
@@ -1,4 +1,4 @@
-import tweepy
+import time
 import ConfigObject
 import sys
 import tweepy
@@ -7,14 +7,15 @@ import pycurl
 from multiprocessing import Pool
 
 cobj = ConfigObject.ConfigObject()
+tweetCounter = 0
 
 #--------------------------------------------------
 # Raise event function to post events to Pulse
 #--------------------------------------------------
 def raiseEvent(eventText, userName, userPwd):
+    print "starting raiseEvent"
     headers = ['Expect:', 'Content-Type: application/json']
     url =  "https://premium-api.boundary.com/v1/events"
-
     newEvent = {
         "fingerprintFields": [
            "@title"
@@ -38,10 +39,78 @@ def raiseEvent(eventText, userName, userPwd):
     c.close()
 
 #---------------------------------------------------
+# Aggregate and send total
+#---------------------------------------------------
+def AggAndSend(sapi):
+   print "starting AggAndSend"
+   print "starting AggAndSend"
+   while True:
+       print "starting AggAndSend"
+       time.sleep(1)
+       print "starting AggAndSend"
+       timestamp = time.mktime(time.localtime())
+       headers = ['Expect:', 'Content-Type: application/json']
+       url =  "https://premium-api.boundary.com/v1/measurements"
+    
+       newMeasure =   {
+        "source": "myserver",
+         "metric": "TweetCount",
+         "measure": sapi.ec,
+       "timestamp": timestamp
+       }
+
+       print "starting pycurl"
+       c= pycurl.Curl()
+       c.setopt(pycurl.URL, url)
+       c.setopt(pycurl.HTTPHEADER,headers )
+       c.setopt(pycurl.CUSTOMREQUEST, "POST")
+       c.setopt(pycurl.USERPWD, cobj.pulseUserPwd)
+       data = json.dumps(newMeasure)
+       c.setopt(pycurl.POSTFIELDS,data)
+       c.perform()
+       print ("metric post status code" +  str(c.getinfo(pycurl.HTTP_CODE)))
+       c.close()
+       sapi.ec = 0
+
+#------------------------------------------------------
+#  Create the metric def in pulsed
+#------------------------------------------------------
+def CreateMetric():
+
+   headers = ['Expect:', 'Content-Type: application/json']
+   url =  "https://premium-api.boundary.com/v1/metrics"
+   newMetric = {
+   "name": "TweetCount",
+   "description": "Tweet Count",
+   "displayName": "Tweet Count",
+   "displayNameShort": "Tweets",
+   "unit": "number",
+   "defaultAggregate": "sum"
+   }
+
+
+   c= pycurl.Curl()
+   c.setopt(pycurl.URL, url)
+   c.setopt(pycurl.HTTPHEADER,headers )
+   c.setopt(pycurl.CUSTOMREQUEST, "POST")
+   c.setopt(pycurl.USERPWD, cobj.pulseUserPwd)
+   data = json.dumps(newMetric)
+   c.setopt(pycurl.POSTFIELDS,data)
+   c.perform()
+   print ("Metric create status code:=" +  str(c.getinfo(pycurl.HTTP_CODE)))
+   c.close()
+
+#---------------------------------------------------
 # Call back for RaiseEvent
 #---------------------------------------------------
 def cb(r):
    print("callback for raiseEvent")
+
+#---------------------------------------------------
+# Call back for RaiseEvent
+#---------------------------------------------------
+def cb2(r):
+   print("callback for measure")
 
 #---------------------------------------------------
 # Create processing pool for raiseEvent
@@ -53,7 +122,7 @@ if __name__ == '__main__':
 # Listener for tweepy stream
 #---------------------------------------------------
 class CustomStreamListener(tweepy.StreamListener):
-
+    ec = 0
     def on_status(self, status):
         print(status.user.screen_name)
         print(status.text)
@@ -65,7 +134,9 @@ class CustomStreamListener(tweepy.StreamListener):
              user = tweet['user']['name']
              text = tweet['text']
              # callPulse(text, user, cobj.pulseUserPwd)
+             # tweetCounter = tweetCounter + 1 
              pool.apply_async(raiseEvent,(text,user,cobj.pulseUserPwd,), callback=cb)
+             self.ec = self.ec + 1
 
     def on_error(self, status_code):
         print >> sys.stderr, 'Encountered error with status code:', status_code
@@ -78,10 +149,16 @@ class CustomStreamListener(tweepy.StreamListener):
 #-------------------------------------------------------
 # Main function
 #-------------------------------------------------------
+CreateMetric()
+
+ec = 0
+pool.apply_async(AggAndSend,(ec,), callback=cb2)
+
 auth = tweepy.OAuthHandler(cobj.consumer_key, cobj.consumer_secret)
 auth.set_access_token(cobj.access_token_key, cobj.access_token_secret)
 # api = tweepy.API(auth)
 sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+sapi.ec = ec
 filterArray = cobj.filterString.split(",")
 sapi.filter(track=filterArray)
 
